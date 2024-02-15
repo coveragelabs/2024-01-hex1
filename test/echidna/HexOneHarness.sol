809b27740a80a8ece91bff7e1518407d43e07bb1
0xnexusflip <0xnexusflip@gmail.com>
2024-02-15 17:32:48 +0000

    finish harnesses for property, add corpus

--- test/echidna/HexOneHarness.sol
+++ test/echidna/HexOneHarness.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import "../../lib/properties/contracts/ERC20/internal/properties/ERC20BasicProperties.sol";
 import "../../src/HexOneToken.sol";
+import "../../lib/properties/contracts/ERC20/internal/properties/ERC20BasicProperties.sol";
+import "../../lib/properties/contracts/ERC20/internal/properties/ERC20BurnableProperties.sol";
 
-contract CryticERC20InternalHarness is HexOneToken, CryticERC20BasicProperties {
-    constructor() {
+contract CryticERC20InternalHarness is HexOneToken, CryticERC20BasicProperties, CryticERC20BurnableProperties {
+    constructor() HexOneToken("HexOneToken", "HEX1") {
         // Setup balances for USER1, USER2 and USER3:
         _mint(USER1, INITIAL_BALANCE);
         _mint(USER2, INITIAL_BALANCE);
--- test/echidna/HexitHarness.sol
+++ test/echidna/HexitHarness.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import "../../lib/properties/contracts/ERC20/internal/properties/ERC20BasicProperties.sol";
 import "../../src/HexitToken.sol";
+import "../../lib/properties/contracts/ERC20/internal/properties/ERC20BasicProperties.sol";
 
 contract CryticERC20InternalHarness is HexitToken, CryticERC20BasicProperties {
-    constructor() {
+    constructor() HexitToken("HexitToken", "HEXIT") {
         // Setup balances for USER1, USER2 and USER3:
         _mint(USER1, INITIAL_BALANCE);
         _mint(USER2, INITIAL_BALANCE);
